4dee874ddeedf471f1dfa275e55133958b85110d
Sergey Bespalko <bespalkoser@gmail.com>
2023-09-13 11:58:41 +0300

    clean up

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -20,37 +20,34 @@ export class AuthComponent {
   private readonly isPassTouched$ = new BehaviorSubject<boolean>(false)
   private readonly sentPass$ = new BehaviorSubject<string | null>(null)
   public readonly password = new FormControl('')
-  private readonly isInvalidPass$ = combineLatest([
-    this.sentPass$,
-    this.store$.pipe(select(getAuthErrorMsg)),
-    this.password.valueChanges.pipe(startWith(null)),
-    this.isPassTouched$
-  ]).pipe(
-    map(([sentPass, message]) => {
-      const pass = this.password
-      const isEmpty = !isNotEmptyStringUtil(pass.value)
-
-      pass.setErrors(null)
-
-      if (isEmpty) {
-        pass.setErrors({ message: PASS_ERROR_REQUIRED_MSG })
-      }
-
-      if (!isEmpty && !!message && pass.value === sentPass) {
-        pass.setErrors({ message })
-      }
-
-      return pass.touched && pass.invalid
-    }),
-    distinctUntilChanged(),
-    takeUntilDestroyed()
-  )
 
   private readonly isPassHiddenSignal = signal(true)
   private readonly inputTypeSignal = computed(() => (this.isPassHidden ? InputTypeEnum.password : InputTypeEnum.text))
   private readonly visibilitySignal = computed(() => (this.isPassHidden ? VisibilityEnum.visibility_off : VisibilityEnum.visibility))
-  private readonly iconColorSignal = toSignal(this.isInvalidPass$.pipe(map(invalid => (invalid ? ICON_COLOR_MAP.red : ICON_COLOR_MAP.grey))))
+  private readonly iconColorSignal = computed(() => (this.isPassInvalid ? ICON_COLOR_MAP.red : ICON_COLOR_MAP.grey))
   private readonly isAuthInProcessSignal = toSignal(this.store$.pipe(select(getIsAuthInProcess)))
+  private readonly isPassInvalidSignal = toSignal(
+    combineLatest([this.sentPass$, this.store$.pipe(select(getAuthErrorMsg)), this.password.valueChanges.pipe(startWith(null)), this.isPassTouched$]).pipe(
+      map(([sentPass, message]) => {
+        const pass = this.password
+        const isEmpty = !isNotEmptyStringUtil(pass.value)
+
+        pass.setErrors(null)
+
+        if (isEmpty) {
+          pass.setErrors({ message: PASS_ERROR_REQUIRED_MSG })
+        }
+
+        if (!isEmpty && !!message && pass.value === sentPass) {
+          pass.setErrors({ message })
+        }
+
+        return pass.touched && pass.invalid
+      }),
+      distinctUntilChanged(),
+      takeUntilDestroyed()
+    )
+  )
 
   public togglePassVisibility(): void {
     this.isPassHiddenSignal.set(!this.isPassHidden)
@@ -70,7 +67,7 @@ export class AuthComponent {
 
     const { value } = this.password
 
-    if (this.password.invalid || !isNotEmptyStringUtil(value) || this.isAuthInProcess) {
+    if (this.isPassInvalid || this.isAuthInProcess || !isNotEmptyStringUtil(value)) {
       return
     }
 
@@ -80,6 +77,10 @@ export class AuthComponent {
 
   public get isAuthInProcess(): boolean {
     return this.isAuthInProcessSignal() ?? false
+  }
+
+  public get isPassInvalid(): boolean {
+    return this.isPassInvalidSignal() ?? false
   }
 
   public get iconColor(): ThemePalette {
