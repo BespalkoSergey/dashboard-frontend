953eb27b3ccef33d269e86a5862a54daa07a2a28
Sergey Bespalko <bespalkoser@gmail.com>
2023-09-15 17:35:30 +0300

    clean up

--- src/app/interceptors/auth-bearer.interceptor.ts
+++ src/app/interceptors/auth-bearer.interceptor.ts
@@ -1,27 +1,15 @@
 import { Injectable } from '@angular/core'
-import { HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HttpInterceptor } from '@angular/common/http'
-import { Observable, catchError, throwError } from 'rxjs'
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http'
+import { Observable } from 'rxjs'
 import { TOKEN_KEY, USER_AUTH_API_URL } from '../constants/constants'
-import { Store } from '@ngrx/store'
-import { authFailed } from '../modules/auth/auth.actions'
 import { LocalStorageService } from '../services/local-storage.service'
 import { isNotEmptyStringUtil } from '../utils/in-not-empty-string.util'
 
 @Injectable()
 export class AuthBearerInterceptor implements HttpInterceptor {
-  public constructor(
-    private readonly store$: Store,
-    private readonly ls: LocalStorageService
-  ) {}
+  public constructor(private readonly ls: LocalStorageService) {}
   public intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(this.getReq(req)).pipe(
-      catchError(e => {
-        if (e instanceof HttpErrorResponse && e.status === 401) {
-          this.store$.dispatch(authFailed({ authErrorMsg: e.message }))
-        }
-        return throwError(e)
-      })
-    )
+    return next.handle(this.getReq(req))
   }
 
   private getReq(req: HttpRequest<unknown>): HttpRequest<unknown> {
